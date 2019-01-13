62b7e08de9c5534725a02391598efb733cd65ffe
pylaa <46619293+pylaa@users.noreply.github.com>
2019-01-13 11:59:55 +0530

    Update APIInterface.java

--- app/src/main/java/com/applemusicarunasreetask/APIInterface.java
+++ app/src/main/java/com/applemusicarunasreetask/APIInterface.java
@@ -6,5 +6,5 @@ import rx.Observable;
 
 public interface APIInterface {
     @GET("v1/us/apple-music/new-releases/all/100/explicit.json")
-    Call<MusicResponse> doGetListResources();
+    Call<MusicResponse> doGetListResources();//
 }

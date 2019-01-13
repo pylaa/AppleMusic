f0dca5e628e3b584de9bef4054eeabbbe5ea0cda
pylaa <46619293+pylaa@users.noreply.github.com>
2019-01-13 12:17:31 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/applemusicarunasreetask/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/applemusicarunasreetask/ExampleInstrumentedTest.java
@@ -22,5 +22,6 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.applemusicarunasreetask", appContext.getPackageName());
+        assertNotEquals("com.apple", appContext.getPackageName());
     }
 }
